d27b472fe26a498febca596e68fc66382ea955bd
YuliyaKor <kiara_lia@mail.ru>
2017-11-22 22:16:59 +0300

    Исправленны отступы 12-21 строк

--- js/stat.js
+++ js/stat.js
@@ -9,17 +9,15 @@ var getColor = function (condition) {
 };
 
 var getMaxItem = function (times) {
-var max = -1;
-var maxIndex = -1;
+  var max = -1;
 
-for (var i = 0; i < times.length; i++) {
-  var time = times[i];
-  if (time > max) {
-    max = time;
-    maxIndex = i;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    }
   }
-}
-  return max;
+    return max;
 };
 
 window.renderStatistics = function (ctx, names, times) {
